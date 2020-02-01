3bcf93c9c398bbe22be6746a2bfdfa34cfdec3a5
Christian Hoene <christian.hoene@symonics.com>
2020-02-01 22:52:18 +0100

    Added json converter

--- src/json2sofa.cpp
+++ src/json2sofa.cpp
@@ -15,6 +15,10 @@
 #include <json-c/json.h>
 #include <ncDim.h>
 #include <ncVar.h>
+#include <netcdf>
+
+using namespace netCDF;
+using namespace netCDF::exceptions;
 
 /************************************************************************************/
 /*!
@@ -23,7 +27,8 @@
  *
  */
 /************************************************************************************/
-static bool TestFileConvention(json_object *jobj, const std::string &filename) {
+static bool TestFileConvention(json_object *jobj, const std::string &filename)
+{
   bool valid = sofa::IsValidSimpleFreeFieldHRIRFile(filename);
   json_object_object_add(
       jobj, "isNetCDF",
@@ -48,79 +53,153 @@ static bool TestFileConvention(json_object *jobj, const std::string &filename) {
   return valid;
 }
 
-/************************************************************************************/
-/*!
- *  @brief          Convert all informations about a NetCDFFile file to JSON
- *
- */
-/************************************************************************************/
-static void DisplayInformations(json_object *jobj,
-                                const std::string &filename) {
-  ///@n this doesnt check whether the file corresponds to SOFA conventions...
-  const sofa::NetCDFFile file(filename);
+const NcDouble myNcDouble;
 
-  //==============================================================================
-  // global attributes
-  //==============================================================================
+//==============================================================================
+// global attributes
+//==============================================================================
+void writeAttributes(json_object *jobj, NcFile &dataFile)
+{
+  json_object_object_foreach(jobj, key, val)
   {
-    json_object *jobj2 = json_object_new_object();
-    json_object_object_add(jobj, "Attributes", jobj2);
-
-    std::vector<std::string> attributeNames;
-    file.GetAllAttributesNames(attributeNames);
-
-    for (std::size_t i = 0; i < attributeNames.size(); i++) {
-      const std::string name = attributeNames[i];
-      const std::string value = file.GetAttributeValueAsString(name);
-
-      json_object_object_add(jobj2, name.c_str(),
-                             json_object_new_string(value.c_str()));
+    switch (json_object_get_type(val))
+    {
+    case json_type_string:
+      dataFile.putAtt(key, json_object_get_string(val));
+      break;
+    default:
+      std::cerr << "Dropping dimensions " << key << std::endl;
     }
   }
+}
 
-  //==============================================================================
-  // dimensions
-  //==============================================================================
+//==============================================================================
+// dimensions
+//==============================================================================
+void writeDimensions(json_object *jobj, NcFile &dataFile)
+{
+  json_object_object_foreach(jobj, key, val)
   {
-    json_object *jobj2 = json_object_new_object();
-    json_object_object_add(jobj, "Dimensions", jobj2);
-
-    std::vector<std::string> dimensionNames;
-    file.GetAllDimensionsNames(dimensionNames);
-
-    for (std::size_t i = 0; i < dimensionNames.size(); i++) {
-      const std::string name = dimensionNames[i];
-      const std::size_t dim = file.GetDimension(name);
-
-      json_object_object_add(jobj2, name.c_str(), json_object_new_int64(dim));
+    switch (json_object_get_type(val))
+    {
+    case json_type_int:
+      dataFile.addDim(key, json_object_get_int(val));
+      break;
+    default:
+      std::cerr << "Dropping attribute " << key << " " << json_object_get_type(val) << std::endl;
     }
   }
+}
 
-  //==============================================================================
-  // variables
-  //==============================================================================
+//==============================================================================
+// variables
+//==============================================================================
+bool writeVariable(std::string name, json_object *jobj, NcFile &dataFile)
+{
+  std::string typeName;
+  std::vector<size_t> dimensions;
+  std::vector<std::string> dimensionNames;
+  std::vector<std::string> attributeNames;
+  std::vector<std::string> attributeValues;
+  std::vector<double> values;
+  
+  json_object_object_foreach(jobj, key, val)
   {
-    json_object *jobj2 = json_object_new_object();
-    json_object_object_add(jobj, "Variables", jobj2);
+    if (!strcmp(key, "TypeName") && json_object_get_type(val) == json_type_string)
+    {
+      typeName = json_object_get_string(val);
+    }
+    else if (!strcmp(key, "Dimensions") && json_object_get_type(val) == json_type_array)
+    {
+      dimensions.reserve(json_object_array_length(val));
 
-    std::vector<std::string> variableNames;
-    file.GetAllVariablesNames(variableNames);
+      for (size_t i = 0; i < json_object_array_length(val); i++)
+      {
+        json_object *node = json_object_array_get_idx(val, i);
+        if (node == nullptr || !json_object_is_type(node, json_type_int))
+        {
+          std::cerr << "variable " << name << " dimension array incorrect " << json_object_get_type(val) << std::endl;
+          return false;
+        }
+        dimensions.push_back(json_object_get_int(node));
+      }
+    }
+    else if (!strcmp(key, "DimensionNames") && json_object_get_type(val) == json_type_array)
+    {
+      for (size_t i = 0; i < json_object_array_length(val); i++)
+      {
+        json_object *node = json_object_array_get_idx(val, i);
+        if (node == nullptr || !json_object_is_type(node, json_type_string))
+        {
+          std::cerr << "variable " << name << " dimensionNames array incorrect " << json_object_get_type(val) << std::endl;
+          return false;
+        }
+                dimensionNames.push_back(json_object_get_string(node));
+      }
+    }
+    else if (!strcmp(key, "Attributes") && json_object_get_type(val) == json_type_object)
+    {
+      json_object_object_foreach(val, akey, aval)
+      {
+        attributeNames.push_back(akey);
+        attributeValues.push_back(json_object_get_string(aval));
+      }
+    }
+    else if (!strcmp(key, "Values") && json_object_get_type(val) == json_type_array)
+    {
+        values.reserve(json_object_array_length(val));
+        for (size_t i = 0; i < json_object_array_length(val); i++) {
+        json_object *node = json_object_array_get_idx(val, i);
+        if (node == nullptr || !(json_object_is_type(node, json_type_int) || json_object_is_type(node, json_type_double)))
+        {
+          std::cerr << "variable " << name << " values incorrect " << i << " " << json_object_get_type(node) << std::endl;
+          return false;
+        }
+        values.push_back(json_object_get_double(node));
+      }
+    }
+    else
+      std::cerr << "variable " << name << " " << key << " " << json_object_get_type(val) << std::endl;
+  }
 
-    for (std::size_t i = 0; i < variableNames.size(); i++) {
-      const std::string name = variableNames[i];
-      json_object *jobj3 = json_object_new_object();
-      json_object_object_add(jobj2, name.c_str(), jobj3);
+  // print
+  std::cerr << name << " typeName " << typeName << std::endl;
+  for (auto i : dimensions)
+    std::cerr << name << " dimension " << i << std::endl;
+  for (auto i : dimensionNames)
+    std::cerr << name << " dimensionName " << i << std::endl;
+  for (auto i : attributeNames)
+    std::cerr << name << " attributeNames " << i << std::endl;
+  for (auto i : attributeValues)
+    std::cerr << name << " attributeValues " << i << std::endl;
+    std::cerr << name << " values " << values.size() << std::endl;
 
-      const std::string typeName = file.GetVariableTypeName(name);
-      json_object_object_add(jobj3, "TypeName",
-                             json_object_new_string(typeName.c_str()));
+  NcVar var = dataFile.addVar(name, typeName, dimensionNames);
+  for (size_t i=0; i < attributeNames.size();i++)
+      var.putAtt(attributeNames[i], attributeValues[i]);
+  var.putVar(values.data());
+  
+  return true;
+}
 
-      std::vector<std::size_t> dims;
-      file.GetVariableDimensions(dims, name);
-      json_object *jarray1 = json_object_new_array();
-      json_object_object_add(jobj3, "Dimensions", jarray1);
-      for (size_t i = 0; i < dims.size(); i++)
-        json_object_array_add(jarray1, json_object_new_int64(dims[i]));
+//==============================================================================
+// variables
+//==============================================================================
+void writeVariables(json_object *jobj, NcFile &dataFile)
+{
+  json_object_object_foreach(jobj, key, val)
+  {
+    switch (json_object_get_type(val))
+    {
+    case json_type_object:
+      writeVariable(key, val, dataFile);
+      break;
+    default:
+      std::cerr << "Dropping variable " << key << " " << json_object_get_type(val) << std::endl;
+    }
+  }
+#if 0
+
 
       std::vector<std::string> dimNames;
       file.GetVariableDimensionsNames(dimNames, name);
@@ -153,6 +232,45 @@ static void DisplayInformations(json_object *jobj,
       for (size_t i = 0; i < values.size();)
         json_object_array_add(jarray3, json_object_new_double(values[i++]));
     }
+#endif
+}
+
+/************************************************************************************/
+/*!
+ *  @brief          Convert all informations about a NetCDFFile file to JSON
+ *
+ */
+/************************************************************************************/
+static void writeSofa(json_object *jobj,
+                      const std::string &filename)
+{
+  try
+  {
+    NcFile dataFile(filename, NcFile::replace);
+
+    json_object_object_foreach(jobj, key, val)
+    {
+      if (json_object_get_type(val) != json_type_object)
+      {
+        std::cerr << "Variable with invalid type " << key << std::endl;
+        continue;
+      }
+      if (!strcmp(key, "Attributes"))
+        writeAttributes(val, dataFile);
+      else if (!strcmp(key, "Dimensions"))
+        writeDimensions(val, dataFile);
+      else if (!strcmp(key, "Variables"))
+        writeVariables(val, dataFile);
+      else
+      {
+        std::cerr << "Unknown variable " << key << std::endl;
+        continue;
+      }
+    }
+  }
+  catch (NcException &e)
+  {
+    e.what();
   }
 }
 
@@ -162,48 +280,167 @@ static void DisplayInformations(json_object *jobj,
  *
  */
 /************************************************************************************/
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   int sz;
   char *filestring;
 
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " hrtf.json" << std::endl;
+  // parse command line
+  if (argc != 3)
+  {
+    std::cerr << "Usage: " << argv[0] << " hrtf.json hrtf.sofa" << std::endl;
     return 1;
   }
 
+  // open file
   FILE *fhd = fopen(argv[1], "rb");
-  if (fhd == NULL) {
+  if (fhd == NULL)
+  {
     std::cerr << "Cannot open file " << argv[1] << std::endl;
     return 2;
   }
+
+  // get file size
   fseek(fhd, 0L, SEEK_END);
   sz = ftell(fhd);
   fseek(fhd, 0L, SEEK_SET);
-
   std::cout << "File " << argv[1] << " has size " << sz << std::endl;
 
-  filestring = (char *)malloc(sz+1);
-  if (!filestring) {
+  // alloc memory for file
+  filestring = (char *)malloc(sz + 1);
+  if (!filestring)
+  {
     std::cerr << "Out of memory" << std::endl;
+    free(filestring);
+    fclose(fhd);
     return 3;
   }
 
-  if (fread(filestring, sz, 1, fhd) != 1) {
+  // read file
+  if (fread(filestring, sz, 1, fhd) != 1)
+  {
     std::cerr << "Cannot read file" << std::endl;
+    free(filestring);
+    fclose(fhd);
     return 4;
   }
+  filestring[sz] = 0;
 
-  filestring[sz]=0;
+  // parse file
   json_object *jobj = json_tokener_parse(filestring);
-  if (jobj == NULL) {
+  if (jobj == NULL)
+  {
     std::cerr << "JSON error" << std::endl;
+    free(filestring);
+    fclose(fhd);
     return 5;
   }
 
-  puts(json_object_to_json_string_ext(jobj, JSON_C_TO_STRING_PRETTY));
-
-  json_object_put(jobj);
+  // free file memory
   free(filestring);
   fclose(fhd);
+
+  // convert to SOFA format
+
+  /*
+function [Obj] = SOFAsave(filename,Obj,varargin)
+%SOFASAVE 
+%   [] = SOFAsave(filename,Obj,Compression) creates a new SOFA file and
+%   writes an entire data set to it.
+%
+%   filename specifies the name of the SOFA file to which the data is written.
+%   Obj is a struct containing the data and meta
+%   data to be written to the SOFA file (see below for exact format).
+%   Compression is an optional numeric value between 0 and 9 specifying the
+%   amount of compression to be applied to the data when writing to the netCDF file.
+%   0 is no compression and 9 is the most compression.
+% 
+%   The existence of mandatory variables will be checked. The dimensions
+%   will be updated.
+
+% SOFA API - function SOFAsave
+% Copyright (C) 2012-2013 Acoustics Research Institute - Austrian Academy of Sciences
+% Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "License")
+% You may not use this work except in compliance with the License.
+% You may obtain a copy of the License at: http://joinup.ec.europa.eu/software/page/eupl
+% Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing  permissions and limitations under the License. 
+
+Def = SOFAdefinitions;
+
+%% check file name
+filename=SOFAcheckFilename(filename);
+
+%% Remove private data
+if isfield(Obj,'PRIVATE'), Obj=rmfield(Obj,'PRIVATE'); end
+
+%% Check convention: mandatory variables
+ObjCheck = SOFAgetConventions(Obj.GLOBAL_SOFAConventions,'m');
+
+varNames = fieldnames(ObjCheck);
+for ii=1:size(varNames,1);
+    if ~isfield(Obj,varNames{ii})
+        error(['Mandatory variable/attribute not existing: ' varNames{ii}]);
+    end
+end
+
+%% Get & set dimensions
+Obj=SOFAupdateDimensions(Obj);
+
+%% Check convention: read-only variables
+ObjCheck = SOFAgetConventions(Obj.GLOBAL_SOFAConventions,'r');
+varNames = fieldnames(ObjCheck);
+
+for ii=1:size(varNames,1);
+	if ischar(Obj.(varNames{ii}))
+		if ~strcmp(Obj.(varNames{ii}), ObjCheck.(varNames{ii}))
+			warning('SOFA:save',[varNames{ii} ' is read-only and was reset to '  ObjCheck.(varNames{ii})],0);
+			Obj.(varNames{ii})=ObjCheck.(varNames{ii});
+		end
+	else
+		if Obj.(varNames{ii}) ~= ObjCheck.(varNames{ii})
+			warning('SOFA:save',[varNames{ii} ' is read-only and was reset to '  ObjCheck.(varNames{ii})],0);
+			Obj.(varNames{ii})=ObjCheck.(varNames{ii});
+		end
+	end
+end
+
+%% check attributes (syntax, 1-dimensional string)
+varNames = fieldnames(Obj);
+for ii=1:size(varNames,1);
+    
+    if size(strfind(varNames{ii},'_'),2) == 1
+        if ~ischar(Obj.(varNames{ii}))
+            error(['Attribute not a valid string: ' varNames{ii} ' = ' num2str(Obj.(varNames{ii}))]);
+        end
+    elseif size(strfind(varNames{ii},'_'),2) > 1
+        error(['Attribute not valid (only one underscore "_" is allowed in attribute name): ' varNames{ii}]);
+    end
+    
+end
+
+%% check varargin (compression)
+if ~isempty(varargin) && isnumeric(varargin{1})
+	if isscalar(varargin{1}) && varargin{1}>=0 && varargin{1}<=9
+        Compression = varargin{1};
+	else
+        error('Error: Compression must be a numeric scalar value between 0 and 9.');
+	end
+else
+    Compression = 1; % default
+end
+
+%% Set/modify time information
+Obj.GLOBAL_DateModified=datestr(now,Def.dateFormat);
+if isempty(Obj.GLOBAL_DateCreated), Obj.GLOBAL_DateCreated=Obj.GLOBAL_DateModified; end
+
+%% Save file
+NETCDFsave(filename,Obj,Compression);
+*/
+
+  //  puts(json_object_to_json_string_ext(jobj, JSON_C_TO_STRING_PRETTY));
+  writeSofa(jobj, argv[2]);
+
+  json_object_put(jobj);
   return 0;
 }
